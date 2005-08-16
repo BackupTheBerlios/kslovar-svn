3bd7affe6ba16877b5d7d0776a1a40b44d2b5b67
mastermind64k <mastermind64k@72194be4-e4fc-0310-9950-f0985a9d8d25>
2005-08-16 11:58:13 +0000

    Minor bugfixes: -Fixed the configuration bug (configuration didn't applied for child widgets)
    Minor source change:
    -Added class Instances, which controlls all instances in the program

--- trunk/src/main.cpp
+++ trunk/src/main.cpp
@@ -28,7 +28,7 @@
 static const char description[] =
     I18N_NOOP("A dictionary for KDE");
 
-static const char version[] = "0.1.0 20050815";
+static const char version[] = "0.1.0 20050816";
 
 static KCmdLineOptions options[] =
 {
